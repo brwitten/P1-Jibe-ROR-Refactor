040a617b6e6da32edf2ff8cbfa8a74e6d480670c
brwitten <bethrwitten@gmail.com>
2017-11-22 15:54:24 -0800

    created show route and updated controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -11,11 +11,20 @@ class TodosController < ApplicationController
   def create
     todo_params = params.require(:todo).permit(:item)
     todo = Todo.new(todo_params)
-
     if todo.save
       redirect_to "/"
     end
   end
 
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
+  def destroy
+    todo_id = params[:id]
+    todo = Todo.find_by_id(todo_id)
+    todo.destroy
+    redirect_to "/"
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   get "/todos", to: "todos#index", as: "todos"
   get "/todos/new", to: "todos#new", as: "new_todo"
   post "/todos", to: "todos#create"
+  get "/todos/:id", to: "todos#show", as: "todo"
+
+  delete "todos/:id", to: "todos#destroy"
 
 end
